ff5403ed6fabc9d9b8b3af009af44141f3a6733c
sebastianbustos2003 <sebastianbustos2003@gmail.com>
2024-09-17 00:10:52 +0000

    prueba 1

--- App-MasterWay/app/index.tsx
+++ App-MasterWay/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hola Mundo.</Text>
     </View>
   );
 }
