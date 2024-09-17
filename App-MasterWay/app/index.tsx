edfe33a7d34f415cab35f18b63b58ae7ebbf8892
sebastianbustos2003 <sebastianbustos2003@gmail.com>
2024-09-17 00:50:05 +0000

    ensayo2

--- App-MasterWay/app/index.tsx
+++ App-MasterWay/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Otra prueba mas.</Text>
+      <Text>Otra prueba mas....</Text>
       
     </View>
   );
