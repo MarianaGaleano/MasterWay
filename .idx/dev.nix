81dc52f2beae3e789aef8dde7061a7c11cea772d
santiagocarde.v06 <santiagocarde.v06@gmail.com>
2024-11-23 17:08:00 +0000

    Actualizaciones

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
 
